fb3720598001e7bf7af8127c193610ecc84e2482
thomasmanho <thomasmanho@yahoo.com.hk>
2015-10-29 16:26:21 +0800

    Added Test case for HashTagActivity

---
+++ app/src/androidTest/java/hk/ust/cse/hunkim/questionroom/HashTagActivityTest.java
@@ -0,0 +1,261 @@
+package hk.ust.cse.hunkim.questionroom;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+
+import org.junit.Before;
+
+import static android.content.Intent.ACTION_VIEW;
+import static android.content.Intent.CATEGORY_DEFAULT;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+
+/**
+ * Created by Thomas on 29/10/2015.
+ */
+public class HashTagActivityTest extends ActivityInstrumentationTestCase2<HashTagActivity> {
+    private HashTagActivity activity;
+
+    public HashTagActivityTest() {
+        super(HashTagActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        Intent mStartIntent = new Intent(getInstrumentation()
+                .getTargetContext(), HashTagActivity.class);
+        mStartIntent.setAction(ACTION_VIEW);
+        mStartIntent.addCategory(CATEGORY_DEFAULT);
+        mStartIntent.setData(Uri.parse("content://qweet.kuenl.com/room/0#0"));
+        setActivityIntent(mStartIntent);
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity = getActivity();
+    }
+
+    public void testNothing() {
+        activity.onOptionsItemSelected(fakeItem);
+    }
+
+    /*
+    public void testHome() {
+        onView(withContentDescription("Navigate up")).perform(click());
+    }
+    */
+    MenuItem fakeItem = new MenuItem() {
+        @Override
+        public int getItemId() {
+            return 0;
+        }
+
+        @Override
+        public int getGroupId() {
+            return 0;
+        }
+
+        @Override
+        public int getOrder() {
+            return 0;
+        }
+
+        @Override
+        public MenuItem setTitle(CharSequence title) {
+            return null;
+        }
+
+        @Override
+        public MenuItem setTitle(int title) {
+            return null;
+        }
+
+        @Override
+        public CharSequence getTitle() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setTitleCondensed(CharSequence title) {
+            return null;
+        }
+
+        @Override
+        public CharSequence getTitleCondensed() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setIcon(Drawable icon) {
+            return null;
+        }
+
+        @Override
+        public MenuItem setIcon(int iconRes) {
+            return null;
+        }
+
+        @Override
+        public Drawable getIcon() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setIntent(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public Intent getIntent() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setShortcut(char numericChar, char alphaChar) {
+            return null;
+        }
+
+        @Override
+        public MenuItem setNumericShortcut(char numericChar) {
+            return null;
+        }
+
+        @Override
+        public char getNumericShortcut() {
+            return 0;
+        }
+
+        @Override
+        public MenuItem setAlphabeticShortcut(char alphaChar) {
+            return null;
+        }
+
+        @Override
+        public char getAlphabeticShortcut() {
+            return 0;
+        }
+
+        @Override
+        public MenuItem setCheckable(boolean checkable) {
+            return null;
+        }
+
+        @Override
+        public boolean isCheckable() {
+            return false;
+        }
+
+        @Override
+        public MenuItem setChecked(boolean checked) {
+            return null;
+        }
+
+        @Override
+        public boolean isChecked() {
+            return false;
+        }
+
+        @Override
+        public MenuItem setVisible(boolean visible) {
+            return null;
+        }
+
+        @Override
+        public boolean isVisible() {
+            return false;
+        }
+
+        @Override
+        public MenuItem setEnabled(boolean enabled) {
+            return null;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean hasSubMenu() {
+            return false;
+        }
+
+        @Override
+        public SubMenu getSubMenu() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setOnMenuItemClickListener(OnMenuItemClickListener menuItemClickListener) {
+            return null;
+        }
+
+        @Override
+        public ContextMenu.ContextMenuInfo getMenuInfo() {
+            return null;
+        }
+
+        @Override
+        public void setShowAsAction(int actionEnum) {
+
+        }
+
+        @Override
+        public MenuItem setShowAsActionFlags(int actionEnum) {
+            return null;
+        }
+
+        @Override
+        public MenuItem setActionView(View view) {
+            return null;
+        }
+
+        @Override
+        public MenuItem setActionView(int resId) {
+            return null;
+        }
+
+        @Override
+        public View getActionView() {
+            return null;
+        }
+
+        @Override
+        public MenuItem setActionProvider(ActionProvider actionProvider) {
+            return null;
+        }
+
+        @Override
+        public ActionProvider getActionProvider() {
+            return null;
+        }
+
+        @Override
+        public boolean expandActionView() {
+            return false;
+        }
+
+        @Override
+        public boolean collapseActionView() {
+            return false;
+        }
+
+        @Override
+        public boolean isActionViewExpanded() {
+            return false;
+        }
+
+        @Override
+        public MenuItem setOnActionExpandListener(OnActionExpandListener listener) {
+            return null;
+        }
+    };
+}
